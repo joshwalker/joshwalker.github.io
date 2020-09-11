83e9050f9484e4323a352ec5a56d8ec97e068d78
Josh Walker <josh.walker@buzzfeed.com>
2020-09-11 13:32:00 -0400

    add link

--- bfp.js
+++ bfp.js
@@ -1,5 +1,5 @@
 window.bfp_native_instream_video = {
     init: function(e) {
-        e.element.innerHTML = "This is the an ad with a "
+        e.element.innerHTML = "This is the an ad with a <a href='https://www.buzzfeed.com'>link</a> in it"
     }
 };
