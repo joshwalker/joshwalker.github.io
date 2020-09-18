f2416f451bca0b06cc9e99b18b0ca9a347b4c6a4
Josh Walker <josh.walker@buzzfeed.com>
2020-09-18 10:50:41 -0400

    add non-video

--- bfp.js
+++ bfp.js
@@ -3,3 +3,9 @@ window.bfp_native_instream_video = {
         e.element.innerHTML = "This is the an ad with a <a href='https://www.buzzfeed.com'>link</a> in it"
     }
 };
+
+window.bfp_shopping_showcase = {
+    init: function(e) {
+        e.element.innerHTML = "This is the a non-video ad with a <a href='https://www.buzzfeed.com'>link</a> in it"
+    }
+};
